82584ae227084ac69e373bd24963bf17b73ed8f0
Neha Purohit <75040563+nehapurohit684@users.noreply.github.com>
2021-03-02 18:39:09 -0500

    Delete js directory

--- js/lib/utitlityFunctions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CalculateFee = void 0;
-function CalculateFee(daysLate) {
-    return daysLate * 0.25;
-}
-exports.CalculateFee = CalculateFee;
-//# sourceMappingURL=utitlityFunctions.js.map
